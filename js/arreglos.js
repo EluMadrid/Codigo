5066486065c4c40dc19ce2e69a88e4dc919e66ba
Elvia Madrid <elvia.madrid97@gmail.com>
2022-10-11 21:51:06 -0500

    retotarea

---
+++ js/arreglos.js
@@ -0,0 +1,21 @@
+//arreglos: son un conjunto de datos 
+const nombres = ["Pepe", "Juan","Luisa", "Maria", "Carlos"]
+//el arreglo nombres tiene 5 elementos 
+
+//indice: es la posicion de cada elemento 
+// Pepe=0
+// Juan =1
+// Luisa =2
+// Maria=3
+// Carlos=4
+
+console.log(nombres[0]); //Pepe
+console.log(nombres[3]); //Maria
+console.log(nombres[5]); //undefined
+
+cont edades = [18, 20, 4,16
+
+edades.push(34);
+edades.push("siete")
+//agregamos un elemento a un arreglo con push, solo se puede uno a la vez
+
